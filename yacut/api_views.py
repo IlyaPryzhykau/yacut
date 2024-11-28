96ab7fc5d681a76ff94fbdbb2a6bd0fce638ee21
Ilya_Pryzhykau <tuarone@gmail.com>
2024-11-28 02:49:13 +0100

    first

---
+++ yacut/api_views.py
@@ -0,0 +1,47 @@
+from flask import jsonify, request, url_for
+
+from . import app, db
+from .error_handlers import InvalidAPIUsage
+from .models import URLMap
+from .views import get_unique_short_id, is_valid_short_url
+
+
+@app.route('/api/id/', methods=['POST'])
+def add_url():
+    """Создает короткую ссылку и сохраняет в базе данных."""
+    raw_data = request.data
+    if raw_data == b'':
+        raise InvalidAPIUsage('Отсутствует тело запроса')
+
+    data = request.get_json()
+    if not data.get('url'):
+        raise InvalidAPIUsage('"url" является обязательным полем!')
+
+    short = data.get('custom_id') or get_unique_short_id()
+    if URLMap.query.filter_by(short=short).first() is not None:
+        raise InvalidAPIUsage(
+            'Предложенный вариант короткой ссылки уже существует.')
+    if not is_valid_short_url(short):
+        raise InvalidAPIUsage('Указано недопустимое имя для короткой ссылки')
+
+    urlmap = URLMap(
+        original=data.get('url'),
+        short=short
+    )
+    db.session.add(urlmap)
+    db.session.commit()
+
+    return jsonify({
+        'url': urlmap.original,
+        'short_link': url_for(
+            'redirect_view', short_url=urlmap.short, _external=True)
+    }), 201
+
+
+@app.route('/api/id/<string:short_id>/', methods=['GET'])
+def get_original_url(short_id):
+    """Получение оригинального URL по короткому идентификатору."""
+    urlmap = URLMap.query.filter_by(short=short_id).first()
+    if urlmap is not None:
+        return jsonify({'url': urlmap.original}), 200
+    raise InvalidAPIUsage('Указанный id не найден', 404)
---
+++ yacut/views.py
@@ -0,0 +1,60 @@
+from random import choices
+import re
+
+from flask import abort, flash, redirect, render_template, url_for
+
+from . import app, db
+from .forms import URLMapForm
+from .models import URLMap
+
+
+CHOICE_STRING = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+                 'abcdefghijklmnopqrstuvwxyz'
+                 '0123456789')
+
+VALID_SHORT_URL_PATTERN = re.compile(r'^[A-Za-z0-9]{1,16}$')
+
+
+def get_unique_short_id():
+    """Генерирует уникальный короткий идентификатор."""
+    while True:
+        short_url = ''.join(choices(CHOICE_STRING, k=6))
+        if URLMap.query.filter_by(short=short_url).first() is None:
+            return short_url
+
+
+def is_valid_short_url(short_url):
+    """Проверяет валидность короткого идентификатора."""
+    return bool(VALID_SHORT_URL_PATTERN.match(short_url))
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index_view():
+    """Обрабатывает GET и POST запросы на главной странице."""
+    form = URLMapForm()
+    if form.validate_on_submit():
+        short_url = form.custom_id.data or get_unique_short_id()
+        if URLMap.query.filter_by(short=short_url).first() is not None:
+            flash('Предложенный вариант короткой ссылки уже существует.')
+            return render_template('urlmap.html', form=form)
+        if not is_valid_short_url(short_url):
+            flash('URL содержит недопустимые символы')
+            return render_template('urlmap.html', form=form)
+        urlmap = URLMap(
+            original=form.original_link.data,
+            short=short_url
+        )
+        db.session.add(urlmap)
+        db.session.commit()
+        return render_template('urlmap.html', form=form,  urlmap=urlmap)
+    return render_template('urlmap.html', form=form)
+
+
+@app.route('/<string:short_url>', methods=['GET'])
+def redirect_view(short_url):
+    """Перенаправляет пользователя на оригинальный URL."""
+    urlmap = URLMap.query.filter_by(short=short_url).first()
+    if urlmap is None:
+        abort(404)
+    original_url = urlmap.original
+    return redirect(original_url)
